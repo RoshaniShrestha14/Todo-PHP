357368c589a71bd78d8ee3bea64bc352cb72de7c
Roshani2211205 <roshani2211205@iimscollege.edu.np>
2025-02-02 18:13:46 +0545

    Update

--- login.php
+++ login.php
@@ -46,10 +46,10 @@
               <input type="email" name="email" id="email" required class="mt-1 block w-full px-3 py-2 rounded-md border-2 border-gray-300">
             </div>
 
-            <!-- <div>
+            <div>
               <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
               <input type="password" name="password" id="password" required class="mt-1 block w-full px-3 py-2 rounded-md border-2 border-gray-300">
-            </div> -->
+            </div>
 
             <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700">Login</button>
           </form>
